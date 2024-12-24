cf87f7a2710de3e125efa1fd3f592adc37c6c520
sushimchaudhary <sushimchaudhary1@gmail.com>
2024-12-24 12:07:25 +0545

    Add files via upload

---
+++ level1.js
@@ -0,0 +1,20 @@
+// --- Comparison operator   --- //
+
+//Q.1 compare  500 with 60, the result should be false
+
+ const a = 500;
+ const b = 60;
+  console.log(a < b);
+
+
+// Q.2 compare '600' with 600, output should be false using equals to
+console.log('600 '=== 600);
+
+// Q.3 compare '600' with 600, output should be true using equals to
+console.log('600' == 600);
+
+// Q.4 compare 50 with 60 using not equals to , the output should be true
+console.log(50 != 60);
+
+// Q.5 compare 70 with 71 using greater or equals to , the output should be true
+console.log(70 <= 71)
